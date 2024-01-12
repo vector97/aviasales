5e0df3675b209bbfb6f2f3444e54f027039dcad8
Viktor Bogatenkov <vector_97@mail.ru>
2024-01-12 22:13:01 +0400

    feat: styled tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,9 +1,32 @@
 import s from './Tabs.module.scss'
 
-console.log('s', s)
+import cn from 'classnames'
 
 function Tabs({ className }) {
-  return <div className={className}>Tabs</div>
+  return (
+    <div className={cn(className, s.tabs)}>
+      <ul className={s.tabs__list}>
+        <li className={s.tabs__item}>
+          <label>
+            <input className={s.tabs__control} type="radio" name="sort" />
+            <span className={s.tabs__label}>Самый дешевый</span>
+          </label>
+        </li>
+        <li className={s.tabs__item}>
+          <label>
+            <input className={s.tabs__control} type="radio" name="sort" />
+            <span className={s.tabs__label}>Самый быстрый</span>
+          </label>
+        </li>
+        <li className={s.tabs__item}>
+          <label>
+            <input className={s.tabs__control} type="radio" name="sort" />
+            <span className={s.tabs__label}>Оптимальный</span>
+          </label>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Tabs
